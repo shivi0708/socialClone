68695899ea223c5eb401ef0764d7726f7e51442c
shivi0708 <shivi.sv8@gmail.com>
2020-08-11 16:48:24 +0530

    uploading files

---
+++ groups/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Group,GroupMember
+# Create your views here.
+from django.contrib.auth.mixins import (LoginRequiredMixin, PermissionRequiredMixin)
+from django.urls import reverse
+from django.views import generic
+
+class CreateGroup(LoginRequiredMixin,generic.CreateView):
+    fields = ('name','description')
+    model = Group
+
+
+class SingleGroup(generic.DetailView):
+    model=Group
+
+
+class ListGroups(generic.ListView):
+    model=Group 
